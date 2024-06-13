6adbd16d0d976058877e42eb872f6faca4bff3ce
Egor <sea25042003@gmail.com>
2024-06-13 16:32:39 +0300

    Added styling to paginator

--- src/pages/TablePage.vue
+++ src/pages/TablePage.vue
@@ -1,7 +1,8 @@
 <template>
   <FilterDate :decrement-date-from="10" @submit="filterUpdated" @clear="filterCleared"></FilterDate>
   <TableTemplate :table-data="tableDataFilteredPaginated" :table-data-header="tableDataHeader"></TableTemplate>
-  <Paginator :rows="rowsPerPage" :first="first" :total-records="totalRecords" @page="updatePage($event)">
+  <Paginator :rows="rowsPerPage" :first="first" :total-records="totalRecords" class="my-paginator"
+    @page="updatePage($event)">
     <template #start="slotProps">
       Показано {{ slotProps.state.first + 1 }} -
       <template v-if="(slotProps.state.page + 1) * slotProps.state.rows > totalRecords">
@@ -97,3 +98,48 @@ export default {
   },
 };
 </script>
+
+
+<style>
+.p-paginator .p-paginator-pages .p-paginator-page:not(:first-child) {
+  border: 1px solid gray;
+  border-radius: 0px;
+  border-left: 0px;
+  margin: 0;
+}
+
+.p-paginator .p-paginator-pages .p-paginator-page:first-child {
+  border: 1px solid gray;
+  border-radius: 5px 0 0 5px;
+  margin: 0;
+
+}
+
+
+
+.p-paginator .p-paginator-pages .p-paginator-page:last-child {
+  border: 1px solid gray;
+  border-left: 0;
+  border-radius: 0 5px 5px 0;
+}
+
+.p-paginator .p-paginator-prev {
+  border: 1px solid gray;
+  border-radius: 5px;
+  margin-right: 12px;
+}
+
+.p-paginator .p-paginator-next {
+  border: 1px solid gray;
+  border-radius: 5px;
+  margin-left: 12px;
+}
+
+.p-paginator .p-paginator-last {
+  display: none;
+}
+
+.p-paginator .p-paginator-first {
+  display: none;
+}
+</style>
